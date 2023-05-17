ce3dd1de6f227cfb639dd872b851d79a1b827a69
Bianca Hellen <60991740+Biahellens@users.noreply.github.com>
2023-05-17 12:20:14 -0300

    create inital page

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,9 +1,47 @@
-import { StepOne } from './pages'
+'use client'
+import React, { useState } from "react"
+import { StepOne, StepTwo, StepThree } from './pages'
 
 export default function Home() {
+  const [btn, setBtn] = useState(1)
   return (
     <div>
-      <StepOne />
+      <div className="grid items-center justify-items-center mt-12">
+        <div className="items-center justify-items-center">
+          <div className="flex w-80 h-12 space-x-4">
+            <button type="submit" onClick={() => setBtn(btn - 1)} className="w-20 h-8 bg-lime-600 text-white rounded-md items-center justify-items-center text-center">
+              Voltar
+            </button>
+            <div className="w-8 h-8 bg-lime-950 text-white rounded-full items-center justify-items-center text-center">
+              <label>
+                1
+              </label>
+            </div>
+            <div className="w-8 h-8 bg-lime-950 text-white rounded-full items-center justify-items-center text-center">
+              <label>
+                2
+              </label>
+            </div>
+            <div className="w-8 h-8 bg-lime-950 text-white rounded-full items-center justify-items-center text-center">
+              <label>
+                3
+              </label>
+            </div>
+            <button type="submit" onClick={() => setBtn(btn + 1)} className="w-20 h-8 bg-lime-700 text-white rounded-md items-center justify-items-center text-center">
+              Próximo
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        {btn == 1 ? (
+          <StepOne />
+          ) : btn == 2 ? (
+            <StepTwo />
+          ):
+            <StepThree />
+        }
+      </div>
     </div>
   )
 }
--- front-end/src/app/pages/index.tsx
+++ front-end/src/app/pages/index.tsx
@@ -1,5 +1,9 @@
 import StepOne from './stepOne'
+import StepTwo from './stepTwo'
+import StepThree from './stepThree'
 
 export {
-  StepOne
+  StepOne,
+  StepTwo,
+  StepThree
 }
