f878b13bab0f6afb1b4eaa92049a381edf677d52
Bianca Hellen <60991740+Biahellens@users.noreply.github.com>
2023-05-18 13:56:52 -0300

    feat: add form

--- src/app/pages/stepOne.tsx
+++ src/app/pages/stepOne.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
 import iconImage from '../../../public/assets/icon-eat-fruit.svg'
 import { Calendar, SelectedUnit, SelectedPeoples, SelectedTime } from '../components'
 
@@ -7,53 +7,53 @@ export default function StepOne() {
   return (
     <div className="grid items-center justify-items-center mt-12">
       <div className="items-center justify-items-center">
-        <div className="w-80">
-          <div className="flex flex-row h-24 bg-lime-200 p-2">
-            <div className="flex">
-              <Image
-                src={iconImage}
-                width={60}
-                height={60}
-                alt="Eat Fruits - icon"
-              />
-            </div>
-            <div className="flex flex-1 content-around">
-              <div className="flex flex-1 mt-4 text-2xl font-semibold text-white justify-center">
-                <label>Reservar Mesa</label>
+        <form>
+          <div className="w-80">
+            <div className="flex flex-row h-24 bg-lime-200 p-2">
+              <div className="flex">
+                <Image
+                  src={iconImage}
+                  width={60}
+                  height={60}
+                  alt="Eat Fruits - icon"
+                />
               </div>
-              <div className="text-xs	text-slate-800">
-                <label>Passo 1 de 3</label>
+              <div className="flex flex-1 content-around">
+                <div className="flex flex-1 mt-4 text-2xl font-semibold text-white justify-center">
+                  <label>Reservar Mesa</label>
+                </div>
+                <div className="text-xs	text-slate-800">
+                  <label>Passo 1 de 3</label>
+                </div>
+              </div>
+            </div>
+            <div className="h-fit bg-lime-100 p-10">
+              <div className="flex flex-col">
+                <div>
+                  <div className="flex flex-col">
+                    <SelectedUnit />
+                  </div>
+
+                  <div className="flex flex-col mt-2">
+                    <SelectedPeoples />
+                  </div>
+
+                  <div className="flex flex-col mt-2">
+                    <SelectedTime />
+                  </div>
+                  <div className="flex flex-col mt-2">
+                    <Calendar />
+                  </div>
+                  <div className="flex justify-center mt-8">
+                      <button type="submit" className="w-48 h-12 rounded-md bg-orange-600 hover:bg-orange-200">
+                          <label className="text-lg font-medium leading-6 text-white">Encontrar Mesas</label>
+                      </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className="h-fit bg-lime-100 p-10">
-            <div className="flex flex-col">
-              <div>
-                <div className="flex flex-col">
-                  <SelectedUnit />
-                </div>
-
-                <div className="flex flex-col mt-2">
-                  <SelectedPeoples />
-                </div>
-
-                <div className="flex flex-col mt-2">
-                  <SelectedTime />
-                </div>
-                <div className="flex flex-col mt-2">
-                  <Calendar />
-                </div>
-                <div className="flex justify-center mt-8">
-                    <button type="submit" className="w-48 h-12 rounded-md bg-orange-600 hover:bg-orange-200">
-                      <Link href="/stepTwo.tsx">
-                        <label className="text-lg font-medium leading-6 text-white">Encontrar Mesas</label>
-                      </Link>
-                    </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   )
